922e11b69d2ef205775946db9c0d7ed87dd8959b
Seymen Bugay <seymenbugay859@gmail.com>
2024-08-14 16:01:09 +0300

    Update sport.js

--- sport.js
+++ sport.js
@@ -22,17 +22,31 @@ function calculateBMI() {
     if (height > 0 && weight > 0) {
         const bmi = (weight / ((height / 100) ** 2)).toFixed(2);
         let result = `Your BMI is ${bmi}. `;
+        const arrow = document.getElementById('arrow');
+
+        // Oku baştan gizle
+        arrow.style.display = 'none';
+        arrow.className = 'bmi-arrow';
 
         if (bmi < 18.5) {
             result += 'You are underweight.';
+            arrow.classList.add('arrow-underweight');
         } else if (bmi >= 18.5 && bmi <= 24.9) {
             result += 'You have a normal weight.';
+            arrow.classList.add('arrow-normal');
         } else if (bmi >= 25 && bmi <= 29.9) {
             result += 'You are overweight.';
-        } else {
+            arrow.classList.add('arrow-overweight');
+        } else if (bmi >= 30 && bmi <= 34.9) {
             result += 'You are obese.';
+            arrow.classList.add('arrow-obese');
+        } else {
+            result += 'You are severely obese.';
+            arrow.classList.add('arrow-severely-obese');
         }
 
+        // Oku göster
+        arrow.style.display = 'block';
         document.getElementById('bmiResult').innerText = result;
     } else {
         document.getElementById('bmiResult').innerText = 'Please enter valid height and weight.';
